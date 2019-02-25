11a700d6ddc1efa9213c4eed644207a1c05ac8a7
itayanna <icage97@gmail.com>
2019-02-25 16:02:45 -0600

    classwork not finished i wil be in study session tomorow

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Account
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Test URL")
+    allAccounts = Account.objects.all()
+
+    context = {
+        'allAccounts': allAccounts
+    }
+
+    return render(request, 'accountApp/index.html', context)
+
+def accountlogin(request):
+    name = Account.name
+    context = {
+        'name': name
+    }
+
+    return render(request, 'accountapp/account.html', context)
